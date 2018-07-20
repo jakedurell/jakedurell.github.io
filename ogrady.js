5df3565d826634697485b810603f96cf9cba85d6
Jake D <jdurell@gmail.com>
2018-07-20 11:37:01 -0400

    typos

--- ogrady.js
+++ ogrady.js
@@ -6,7 +6,7 @@ let para1 = "Dear Attorney O'Grady,"
 
 let para2 = "I am a Vermont citizen writing to request access to my records, currently being held in trust for me by the Vermont General Assembly (the 'Legislature'). I am making this request to the Legislature pursuant to Title I, Chapter 5, Subchapter 3, of the Vermont Public Records Act, and pursuant to Chapter I, Article 6 of the Vermont Constitution."
 
-let para3 = "Specifically, I would like a copy of all electronic records falling within the following categories or containing any information falling within the following:"
+let para3 = "Specifically, I would like a copy of all electronic records falling within the following categories or containing any information falling within the following categories:"
 
 let para4 = "I am requesting all records as of the date and time provided at the top of the body of this email ('Request Time'). To the extent any records requested are impermanent and records existing at the Request Time cannot be provided, I am requesting that records existing as close in time as possible to the Request Time be provided. I am expecting the fulfillment of this request within the statutorily required three (3) business days under 1 V.S.A. S.318(a)(2)."
 
@@ -14,7 +14,7 @@ let para5 = "I am requesting that all such records be produced in their original
 
 let para6 = "This e-mail is not and shall not be construed to be an agreement to pay any costs in retrieving such records. Estimated costs will be promptly considered. Any inflation of estimated costs will likely require substantial attorney expense to address, for which I would seek compensation. Such inflation includes, but is not limited to assesments for (1) costs arising from removing or filtering purportedly exempt data from otherwise very public data; (2) costs arising from querying or accessing information when such queries or access protocol already exists within the General Assembly on its websites or otherwise; and (3) costs arising from querying or accessing information which is not stored according to reasonable business standards."
 
-let para7 = "Thank you for your assistance in this matter. Please understand that I am open to reasonable extensions and refinements of my request if may lead to achieving my core information objectives, which I am happy to discuss with you. Please feel free to contact me if you have any questions."
+let para7 = "Thank you for your assistance in this matter. Please understand that I am open to reasonable extensions and refinements of my request if it may lead to achieving my core information objectives, which I am happy to discuss with you. Please feel free to contact me if you have any questions."
 
 let closing = "Sincerely,"
 
